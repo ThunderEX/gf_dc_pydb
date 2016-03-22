1be019d76efa84361e1924fd4a07817fc9f1163e
szwchao <szwchao@gmail.com>
2016-03-22 14:19:20 +0800

    Add fw_upgrader in run

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This tool is developed by Python language. It can auto generate data and insert 
 
 ## How to use
 
-1. Download Python from [here](https://www.python.org/downloads/) and install. Please use Python2, it is not compatible with Python3. Recommend to use Python 2.7.9 or later.
+1. Download Python from [here](https://www.python.org/downloads/) and install. Support both Python2.x and Python3.x
 2. Copy the script folder to DC project root directory.
 3. Open windows cmd window in this tool directory and run: `python run.py` (make sure python.exe is added in your environment variable)
 
--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 from scripts.misc import *
 from scripts.util.log import *
 import scripts.feature as f
-# from scripts.util.fw_upgrader.src.fw_upgrader import fw_upgrader, guess_local_ip
+from scripts.util.fw_upgrader.src.fw_upgrader import fw_upgrader, guess_local_ip
 
 def update_database():
     copy_database()
@@ -20,11 +20,11 @@ def build():
     ghs_build('rebuild')
     generate_firmware()
 
-# def download():
-    # fw_upgrader('c:/local/Work/tools/tftpd64/', guess_local_ip(), '10.208.32.133')
+def download():
+    fw_upgrader('c:/local/Work/tools/tftpd64/', guess_local_ip(), '10.208.32.133')
 
 if __name__ == '__main__':
     # change_software_version('Vx3.17.02')
-    update_database()
+    # update_database()
     # build()
-    # download()
+    download()
