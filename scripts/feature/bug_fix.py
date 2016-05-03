36ab38e0cac3f08c5aafea773b81d34fbccbef1f
szwchao <szwchao@gmail.com>
2016-05-03 10:52:22 +0800

    update fw_upgrader
    fix a bug that need to add an available rule in vfd AO

--- scripts/feature/bug_fix.py
+++ scripts/feature/bug_fix.py
@@ -38,3 +38,32 @@ def mp204_current_numbers():
     '''
     _id = DisplayComponent().get(Name='1.2 PumpStatus1 l1 mp204 current NQ').id
     DisplayNumberQuantity().update(id=_id, NumberOfDigits=5)
+
+def vfd_ao_available():
+    table = DisplayListViewColumns(ListViewId="4.4.3.1 AnalogOutputSetup List 1 func", ColumnIndex=3, ColumnWidth=0)
+    table.add()
+
+    table = DisplayListViewItem(ListViewId="4.4.3.1 AnalogOutputSetup List 1 func", Index=3)
+    r = table.get(ListViewId=table.model.ListViewId, Index=3)
+    table = DisplayListViewItemComponents(ListViewItemId=r.id, ColumnIndex=3, ComponentId="Availability rule: VFD 2 installed")
+    table.add()
+
+    table = DisplayListViewItem(ListViewId="4.4.3.1 AnalogOutputSetup List 1 func", Index=4)
+    r = table.get(ListViewId=table.model.ListViewId, Index=4)
+    table = DisplayListViewItemComponents(ListViewItemId=r.id, ColumnIndex=3, ComponentId="Availability rule: VFD 3 installed")
+    table.add()
+
+    table = DisplayListViewItem(ListViewId="4.4.3.1 AnalogOutputSetup List 1 func", Index=5)
+    r = table.get(ListViewId=table.model.ListViewId, Index=5)
+    table = DisplayListViewItemComponents(ListViewItemId=r.id, ColumnIndex=3, ComponentId="Availability rule: VFD 4 installed")
+    table.add()
+
+    table = DisplayListViewItem(ListViewId="4.4.3.1 AnalogOutputSetup List 1 func", Index=6)
+    r = table.get(ListViewId=table.model.ListViewId, Index=6)
+    table = DisplayListViewItemComponents(ListViewItemId=r.id, ColumnIndex=3, ComponentId="Availability rule: VFD 5 installed")
+    table.add()
+
+    table = DisplayListViewItem(ListViewId="4.4.3.1 AnalogOutputSetup List 1 func", Index=7)
+    r = table.get(ListViewId=table.model.ListViewId, Index=7)
+    table = DisplayListViewItemComponents(ListViewItemId=r.id, ColumnIndex=3, ComponentId="Availability rule: VFD 6 installed")
+    table.add()
